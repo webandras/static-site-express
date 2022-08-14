cf438f84d8444abae75a80b53aa54ccce6ef8a74
Gulácsi András <guland@protonmail.com>
2022-08-14 16:35:18 +0200

    update: Global site config

--- config/site.config.js
+++ config/site.config.js
@@ -1,15 +1,16 @@
 // Add your global site properties here
 module.exports = {
   site: {
-    url: 'https://static-site-express.netlify.com/',
+    url: "http://localhost:4000",
     title: `static-site-express`,
-    defaultImage: '/assets/images/static.jpg',
-    monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    lang: 'en',
-    author: 'András Gulácsi',
-    quote: 'A Node.js-based static site generator that uses EJS and Markdown',
-    description: 'A Node.js-based static site generator that uses EJS and Markdown',
+    defaultImage: "/assets/images/static.jpg",
+    monthNames: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    lang: "en",
+    author: "András Gulácsi",
+    quote: "A Node.js-based static site generator that uses EJS and Markdown",
+    description:
+      "A Node.js based static-site generator that uses EJS and Markdown. Deploy your static site to Netlify or any platform to your liking. Suited for landing pages, portfolio, blogs, documentation, hobby projects.",
     currentYear: new Date().getFullYear(),
-    github: 'https://github.com/SalsaBoy990'
-  }
-}
+    github: "https://github.com/SalsaBoy990/static-site-express",
+  },
+};
