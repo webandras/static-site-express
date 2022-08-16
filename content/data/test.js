e83cc680881425fae10320213e84b5076d7de229
Gulácsi András <guland@protonmail.com>
2022-08-16 11:44:42 +0200

    fix: Opening hours

--- content/data/test.js
+++ content/data/test.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 // Get document, or throw exception on error
 try {
   const doc = yaml.load(fs.readFileSync("opening-hours.yml", "utf8"));
-  console.log(doc.Monday);
+  console.log(doc.monday[0]);
 } catch (e) {
   console.log(e);
 }
