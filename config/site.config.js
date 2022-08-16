603e428af48b74ff2734dd79cb848da318da864a
Gulácsi András <guland@protonmail.com>
2022-08-16 11:25:17 +0200

    fix: Opening hours file read

--- config/site.config.js
+++ config/site.config.js
@@ -1,65 +1,67 @@
 const yaml = require("js-yaml");
 const fs = require("fs");
 
-// Get opening hours data from yaml file
 try {
-  const openingHours = yaml.load(fs.readFileSync("./../content/data/opening-hours.yml", "utf8"));
+  // Get opening hours data from yaml file
+  const openingHours = yaml.load(fs.readFileSync("./content/data/opening-hours.yml", "utf8"));
+  console.log(openingHours);
+
+  // Add your global site properties here
+  module.exports = {
+    site: {
+      // BASE
+      title: `static-site-express`,
+      quote: "A Node.js-based static site generator that uses EJS and Markdown",
+      description:
+        "A Node.js based static-site generator that uses EJS and Markdown. Deploy your static site to Netlify or any platform to your liking. Suited for landing pages, portfolio, blogs, documentation, hobby projects.",
+      author: "András Gulácsi",
+      lang: "en", // currently, the static site generator has no multi-language support
+      defaultImage: "/assets/images/static.jpg",
+      github: "https://github.com/SalsaBoy990/static-site-express",
+      currentYear: new Date().getFullYear(),
+
+      // CONFIGURATION
+
+      // for local development, leave it empty or use http://localhost:PORT (without trailing "/" at the end!)
+      // This url is pasted before all of your "/links" in the website
+      url: "",
+
+      // Used it for creating canonical urls
+      seoUrl: "https://static-site-express.netlify.app",
+
+      // Using Algolia Search requires configuration
+      // For local development set it to false (no need to send in the postdata for the search index)
+      enableSearch: true,
+
+      // Change these to the language you want to use
+      monthNames: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+
+      hours: {
+        "Monday": openingHours.Monday|| {},
+        "Tuesday": openingHours.Tuesday|| {},
+        "Wednesday": openingHours.Wednesday|| {},
+        "Thursday": openingHours.Thursday|| {},
+        "Friday": openingHours.Friday|| {},
+        "Saturday": openingHours.Saturday || {},
+        "Sunday": openingHours.Sunday || {},
+      },
+
+      // Switch website to the dark mode
+      darkMode: true,
+
+      // Link for the deploy button url
+      netlifyDeployLink: "https://app.netlify.com/start/deploy?repository=https://github.com/SalsaBoy990/static-site-express",
+      netlifyDeployButtonId: "#deploy",
+
+      // Supported languages (used for i18n)
+      languages: {
+        hu: { nativeName: "Magyar" },
+        en: { nativeName: "English" },
+      },
+
+    },
+  };
+
 } catch (e) {
   console.log(e);
 }
-
-// Add your global site properties here
-module.exports = {
-  site: {
-    // BASE
-    title: `static-site-express`,
-    quote: "A Node.js-based static site generator that uses EJS and Markdown",
-    description:
-      "A Node.js based static-site generator that uses EJS and Markdown. Deploy your static site to Netlify or any platform to your liking. Suited for landing pages, portfolio, blogs, documentation, hobby projects.",
-    author: "András Gulácsi",
-    lang: "en", // currently, the static site generator has no multi-language support
-    defaultImage: "/assets/images/static.jpg",
-    github: "https://github.com/SalsaBoy990/static-site-express",
-    currentYear: new Date().getFullYear(),
-
-    // CONFIGURATION
-
-    // for local development, leave it empty or use http://localhost:PORT (without trailing "/" at the end!)
-    // This url is pasted before all of your "/links" in the website
-    url: "",
-
-    // Used it for creating canonical urls
-    seoUrl: "https://static-site-express.netlify.app",
-
-    // Using Algolia Search requires configuration
-    // For local development set it to false (no need to send in the postdata for the search index)
-    enableSearch: true,
-
-    // Change these to the language you want to use
-    monthNames: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-
-    hours: {
-      "Monday": openingHours.Monday|| {},
-      "Tuesday": openingHours.Tuesday|| {},
-      "Wednesday": openingHours.Wednesday|| {},
-      "Thursday": openingHours.Thursday|| {},
-      "Friday": openingHours.Friday|| {},
-      "Saturday": openingHours.Saturday || {},
-      "Sunday": openingHours.Sunday || {},
-    },
-
-    // Switch website to the dark mode
-    darkMode: true,
-
-    // Link for the deploy button url
-    netlifyDeployLink: "https://app.netlify.com/start/deploy?repository=https://github.com/SalsaBoy990/static-site-express",
-    netlifyDeployButtonId: "#deploy",
-
-    // Supported languages (used for i18n)
-    languages: {
-      hu: { nativeName: "Magyar" },
-      en: { nativeName: "English" },
-    },
-
-  },
-};
