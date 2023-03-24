3275bd285d492d7d6489b6bcfbd0e3cbf95d9070
Gulácsi András <guland@protonmail.com>
2023-03-24 22:38:39 +0100

    update: Webpack configuration, update packages (remove, move to dev-dependencies, add browser-sync), add npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./assets/js/main.js",
@@ -7,8 +8,22 @@ module.exports = {
     path: `${__dirname}/content/assets/js`,
     filename: "main.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      }
+    ]
+  },
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './../css/[name].css',
+      // filename: './../css/[name].[contenthash].css',
+    }),
+  ]
 };
