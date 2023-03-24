b89f472cbe87285e53916c51ac2a11eaf74ea16b
Gulácsi András <guland@protonmail.com>
2023-03-24 22:54:10 +0100

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,10 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
 const cssnano = require("cssnano");
 
 module.exports = {
   plugins: [
-    require("tailwindcss"),
     require("autoprefixer"),
     cssnano({
       preset: "default",
     }),
   ],
 };
-
-// https://flaviocopes.com/tailwind-setup/
